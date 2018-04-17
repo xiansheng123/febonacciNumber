85966d12ced655bb30064aee61c89fafd8fa7ce6
xiansheng123 <732824600@qq.com>
2018-04-18 00:28:17 +0800

    add Chris Tree

--- src/main/java/com/example/febonacci/FebonacciApplication.java
+++ src/main/java/com/example/febonacci/FebonacciApplication.java
@@ -8,13 +8,14 @@ import java.math.BigInteger;
 public class FebonacciApplication {
 
     public static void main(String[] args) {
-        febonacci1 (100);
+        //febonacci1 (100);
+        chrisTree ();
     }
 
     private static void febonacci1(int number) {
         BigInteger a = new BigInteger ("0");
         BigInteger b = new BigInteger ("1");
-        BigInteger c = new BigInteger ("1");
+        BigInteger c;
         StringBuilder sb = new StringBuilder ();
 
         for (int i = 0; i <= number; i++) {
@@ -29,4 +30,36 @@ public class FebonacciApplication {
         }
         System.out.println (sb.toString ());
     }
+
+    //  an= a1+(n-1)*d
+    private static void chrisTree() {
+        for (int i = 1; i <= 5; i++) {
+            printStart (1 + (i - 1) * 2, i, 5);
+        }
+        for (int i = 1; i <= 5; i++) {
+            printStart (1 + (i - 1) * 2, i, 5);
+        }
+        System.out.println (printSpace (1, 5).append ("*"));
+        System.out.println (printSpace (1, 5).append ("*"));
+        System.out.println (printSpace (1, 5).append ("*"));
+    }
+
+    private static StringBuilder printSpace(int LineNo, int max) {
+        StringBuilder sb = new StringBuilder ();
+        int numberOfSpace = max - LineNo;
+        for (int j = 0; j < numberOfSpace; j++) {
+            sb.append (" ");
+        }
+        return sb;
+    }
+
+    private static void printStart(int i, int lineNo, int max) {
+
+        StringBuilder sb = printSpace (lineNo, max);
+        for (int j = 0; j < i; j++) {
+            sb.append ("*");
+        }
+        System.out.println (sb.toString ());
+    }
+
 }
